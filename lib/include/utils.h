a96fd6ab5de73372f3fb418073bf74f311b9b093
Israel de Simas <israel.de.simas@gmail.com>
2019-06-29 15:09:53 -0300

    Primeiro commit da biblioteca com funções uteis e genéricas

---
+++ lib/include/utils.h
@@ -0,0 +1,63 @@
+/**************************************************************************
+ * utils.h
+ *
+ *  Create on: 21/06/2019
+ *
+ *  Header for utils tasks
+ *
+ * Copyrights, 2019
+ *
+ **************************************************************************/
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+/**************************************************************************
+ * INCLUDES
+ **************************************************************************/
+#include <syslog.h>
+
+/**************************************************************************
+ * DEFINITIONS
+ **************************************************************************/
+
+#define LENGTH_DATETIME_PARAM	40
+#define USLEEP_MAX            (1000000 - 1)
+
+#define TRUE      1
+#define FALSE     0
+
+#define ERROR	    -1
+#define SUCCESS	  0
+
+#define EMPTY_VALUE		0x00
+#define POINTER_NULL  '\0'
+
+#define INVALID_FUNCTION_RET 	-1
+
+#define log(str,...)      	PRINT_LOG((LOG_DEBUG | LOG_INFO),str,##__VA_ARGS__)
+#define log_error(str,...)  PRINT_ERROR_LOG(LOG_ERR,str,##__VA_ARGS__)
+
+#if 1 // SYSLOG or PRINTF
+  #define PRINT_LOG(out,str,...)                syslog(out, "%s | %s() | "str" | %d", THIS_FILE,__FUNCTION__,##__VA_ARGS__,__LINE__)
+  #define PRINT_ERROR_LOG(out,str,...)          syslog(out, "#Error# %s | %s() | "str" | %d", THIS_FILE,__FUNCTION__,##__VA_ARGS__,__LINE__)
+#else
+	#define PRINT_LOG(out,str,...)                printf("%s | %s() | "str" | %d\n",THIS_FILE,__FUNCTION__,##__VA_ARGS__,__LINE__)
+  #define PRINT_ERROR_LOG(out,str,...)          printf("#Error# %s | %s() | "str" | %d\n",THIS_FILE,__FUNCTION__,##__VA_ARGS__,__LINE__)
+#endif
+
+/*************************************************************************
+ * LIB FUNCTIONS
+ *************************************************************************/
+int msleep(int nMsec);
+
+void logOpen(char *pchLogAppName, int facility);
+
+void logClose();
+
+void resetBoard();
+
+void restoreConfig();
+
+void retrieveDateAndTime(struct tm *pTmCallTime, char *pchDateTime);
+
+#endif
---
+++ lib/src/utils.c
@@ -0,0 +1,59 @@
+/**************************************************************************
+ *
+ * utils.c
+ *
+ *    Utils functions for platform
+ *
+ * Copyright 2019
+ *
+ **************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <utils.h>
+
+#define THIS_FILE "utils.c"
+
+int msleep(int nMsec) {
+  
+  int sleepTime = nMsec * 1000;
+
+  if ((sleepTime >= 0) && (sleepTime <= USLEEP_MAX)) {
+    return usleep(sleepTime);
+  } else if (nMsec >= 0) {
+    return sleep(nMsec/1000);
+  } else {
+    return -1;
+  }
+}
+
+void logOpen(char *pchLogAppName, int facility) {
+  openlog(pchLogAppName, 0, facility);
+}
+
+void logClose() {
+  closelog();
+}
+
+void resetBoard() {
+	char *pcCommand[] = {"reboot", NULL};
+
+  execvp(pcCommand[0], pcCommand);
+}
+
+void restoreConfig() {
+	system("rm -rf /data/*");
+	system("sync");
+	system("/etc/init.d/files_default start");
+	system("/etc/init.d/mac start");
+	system("sync");
+	system("reboot");
+}
+
+void retrieveDateAndTime(struct tm *pTmCallTime, char *pchDateTime) {
+  // Format 'YYYY-MM-DD HH:MM:SS'
+  strftime(pchDateTime, LENGTH_DATETIME_PARAM, "%Y-%m-%d %H:%M:%S", pTmCallTime);
+}
