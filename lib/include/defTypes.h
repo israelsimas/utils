212ec407ae68cb9e95213caa8c600e0a4b10ed69
Your Name <you@example.com>
2019-07-06 10:14:57 -0300

    Suporte a instalação

---
+++ lib/include/defTypes.h
@@ -0,0 +1,29 @@
+/**************************************************************************
+ * defTypes.h
+ *
+ *  Create on: 31/08/2019
+ *
+ * Copyrights 2019
+ *
+ **************************************************************************/
+#ifndef __DEFTYPES_H__
+#define __DEFTYPES_H__
+
+typedef unsigned char 	BYTE; 	// 1 byte
+typedef unsigned short 	WORD; 	// 2 bytes
+typedef unsigned long 	DWORD; 	// 4 bytes
+typedef unsigned long 	ULONG; 	// 8 bytes
+typedef unsigned int	  UINT; 	// 8 bytes
+typedef int 			      BOOL;
+typedef long 			      LONG;
+typedef long 			      STATUS;
+typedef float 			    FLOAT;
+typedef double 			    DOUBLE;
+typedef signed char     S8;
+typedef signed short    S16;
+
+/* Timers */
+typedef DWORD 			    TIME;
+typedef WORD 			      TIMEL;
+
+#endif 
